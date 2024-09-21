6c69dd3c042000054dae96c15161d0cfe345a063
Joaquin Sateler <86966872+cacosat@users.noreply.github.com>
2024-09-21 11:35:06 -0300

    app.js tweaks

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
+require('dotenv').config();
 const express = require('express');
 const app = express();
-const port = 3001;
+const PORT = process.env.PORT || 3000;
+const HOST = process.env.HOST;
 
 // app.METHOD(PATH, HANDLER)
 app.get('/api', (req, res) => {
-    res.send('Hello World! From /api')
+    res.send(`Hello World! From ${HOST}:${PORT}/api`)
 })
 
-app.listen(port || 3000, () => {
-    console.log(`app listening on ${port} or (||) 3000`)
+app.listen(PORT, () => {
+    console.log(`app listening on ${HOST}:${PORT} or (||) 3000`)
 })
